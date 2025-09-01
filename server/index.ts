69ab334f23cef0329c61859ea60b64b1f4c1ce08
Nishan Bajracharya <nisbaj11@gmail.com>
2025-09-01 17:52:17 +0545

    Set up basic api routing

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 
+import router from './routes';
+
 const app = express();
 const port = process.env.PORT || 8081;
-
-// Get the directory name using the modern ES module method
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 // Serve the static files from the frontend's build directory
 const frontendBuildPath = 'client/dist';
@@ -13,9 +11,7 @@ const frontendBuildPath = 'client/dist';
 app.use(express.static(frontendBuildPath));
 
 // Your API routes should be prefixed to avoid conflicts with frontend routes
-app.get('/api/status', (req, res) => {
-  res.json({ message: 'API is running!' });
-});
+app.use('/api', router);
 
 // A catch-all route to serve the main index.html for client-side routing
 app.get(/(.*)/, (req, res) => {
